5675f29d108a2b3d49413bf51256486e2ff521fe
quanganh@infinitelambda.com <quanganh@infinitelambda.com>
2022-01-10 09:04:52 +0000

    lifetime value

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -2,17 +2,36 @@ with customers as (
     select * from {{ ref('staging_customers') }}
 ),
 
+payments as (
+    select order_id,
+        sum(amount) as amount
+
+    from {{ ref('staging_payments') }}
+    group by order_id 
+),
+
 orders as (
     select * from {{ ref('staging_orders') }}
+),
+
+order_payment as (
+    select orders.customer_id,
+        orders.order_date,
+        orders.order_id,
+        coalesce(payments.amount, 0) as amount
+        
+        from orders
+        left join payments using (order_id)
 ),
 
 customer_orders as (
     select customer_id,
     max(order_date) as last_order_date,
     min(order_date) as first_order_date,
-    count(order_id) as number_of_orders
+    count(order_id) as number_of_orders,
+    sum(amount) as lifetime_value
 
-    from orders
+    from order_payment
     group by customer_id
 ),
 
@@ -22,6 +41,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.last_order_date,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
         from customers

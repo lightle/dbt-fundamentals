af45c587faae6bdcedc7d0f2c824be970af0d9ad
quanganh@infinitelambda.com <quanganh@infinitelambda.com>
2022-01-14 10:24:51 +0000

    customer lifetime value

--- models/marts/core/fact_customer_orders.sql
+++ models/marts/core/fact_customer_orders.sql
@@ -1,10 +1,5 @@
-with order_payment as (
-    select order_id,
-        max(payment_date) as payment_finalized_date,
-        sum(amount) / 100.0 as total_amount_paid
-    from {{ ref('stg_payment') }}
-    where payment_status <> 'fail'
-    group by 1
+with payment as (
+    select * from {{ ref ('stg_payment') }}
 ),
 
 orders as (
@@ -13,6 +8,15 @@ orders as (
 
 customers as (
     select * from {{ ref('stg_customers') }}
+),
+
+order_payment as (
+    select order_id,
+        max(payment_date) as payment_finalized_date,
+        sum(amount) / 100.0 as total_amount_paid
+    from payment
+    where payment_status <> 'fail'
+    group by 1
 ),
 
 paid_orders as (
@@ -29,15 +33,20 @@ left join order_payment using (order_id)
 left join customers using (customer_id)
 ),
 
-customer_orders 
-as (select C.ID as customer_id
-    , min(ORDER_DATE) as first_order_date
-    , max(ORDER_DATE) as most_recent_order_date
-    , count(ORDERS.ID) AS number_of_orders
-from raw.jshop.customers C 
-left join raw.jshop.orders as Orders
-on orders.USER_ID = C.ID 
-group by 1)
+customer_orders as (
+    select customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) AS number_of_orders
+    from orders
+    group by 1
+)
+
+lifetime_value as (
+    select order_id,
+        sum(total_amount_paid) over (partition by customer_id order by order_id asc) as customer_lifetime_value
+    from paid_orders
+)
 
 select
 p.*,
@@ -50,14 +59,5 @@ x.clv_bad as customer_lifetime_value,
 c.first_order_date as fdos
 FROM paid_orders p
 left join customer_orders as c USING (customer_id)
-LEFT OUTER JOIN 
-(
-        select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
+left outer join customer_lifetime_value using (order_id)
 ORDER BY order_id

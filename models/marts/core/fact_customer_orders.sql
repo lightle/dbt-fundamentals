54c9f776903a89df327d1644724a506d2e1dca0c
quanganh@infinitelambda.com <quanganh@infinitelambda.com>
2022-01-14 10:41:03 +0000

    fact_customer_orders refactored

--- models/marts/core/fact_customer_orders.sql
+++ models/marts/core/fact_customer_orders.sql
@@ -35,29 +35,41 @@ left join customers using (customer_id)
 
 customer_orders as (
     select customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
+        min(order_placed_at) as first_order_date,
+        max(order_placed_at) as most_recent_order_date,
         count(order_id) AS number_of_orders
     from orders
     group by 1
-)
+),
 
 lifetime_value as (
     select order_id,
         sum(total_amount_paid) over (partition by customer_id order by order_id asc) as customer_lifetime_value
     from paid_orders
+),
+
+final as (
+    select order_id,
+        customer_id,
+        order_placed_at,
+        order_status,
+        total_amount_paid,
+        payment_finalized_date,
+        customer_first_name,
+        customer_last_name,
+        ROW_NUMBER() OVER (ORDER BY order_id) as transaction_seq,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_id) as customer_sales_seq,
+        CASE 
+            WHEN first_order_date = order_placed_at
+            THEN 'new'
+            ELSE 'return'
+            END as nvsr,
+        customer_lifetime_value,
+        first_order_date as fdos
+    FROM paid_orders p
+    left join customer_orders as c USING (customer_id)
+    left outer join lifetime_value using (order_id)
+    ORDER BY order_id
 )
 
-select
-p.*,
-ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
-ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
-CASE WHEN c.first_order_date = p.order_placed_at
-THEN 'new'
-ELSE 'return' END as nvsr,
-x.clv_bad as customer_lifetime_value,
-c.first_order_date as fdos
-FROM paid_orders p
-left join customer_orders as c USING (customer_id)
-left outer join customer_lifetime_value using (order_id)
-ORDER BY order_id
+select * from final
